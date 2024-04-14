22cb3dc3fde3d194d84f2bcad48097c1ec2bbee0
Paul-Maxime <github@paulmaxime.fr>
2024-04-14 02:43:59 +0200

    Update the test scene to show the entites as the godot logo

--- client/scripts/TestGame.gd
+++ client/scripts/TestGame.gd
@@ -1,6 +1,9 @@
 extends Node
 
-var rng = RandomNumberGenerator.new()
+var entity_scene: PackedScene = preload("res://scenes/TestEntity.tscn")
+
+var _rng = RandomNumberGenerator.new()
+var _entities = {};
 
 func _ready():
 	$Network.game_joined.connect(_on_game_joined)
@@ -10,13 +13,28 @@ func _ready():
 
 func _on_game_joined(player_id: int):
 	print("Joined the game, player id: %d" % [player_id])
-	$Network.sendSummon(rng.randi() % 512, 0 if player_id % 2 == 0 else 511)
+	$Network.sendSummon(_rng.randi() % 512, 0 if player_id % 2 == 0 else 511)
 
 func _on_entity_summoned(entity_id: int, owner_id: int, x: int, y: int):
 	print("Entity %d summoned at (%d, %d), owner %d" % [entity_id, x, y, owner_id])
+	var entity: Node2D = entity_scene.instantiate()
+	entity.position = Vector2(x, y)
+	_entities[entity_id] = entity
+	$Entities.add_child(entity)
 
 func _on_entity_moved(entity_id: int, x: int, y: int):
 	print("Entity %d moved to (%d, %d)" % [entity_id, x, y])
+	var entity = _entities[entity_id]
+	if entity == null:
+		print("Unknown entity")
+		return
+	entity.position = Vector2(x, y)
 
 func _on_entity_despawned(entity_id: int):
 	print("Entity %d despawned" % [entity_id])
+	var entity = _entities[entity_id]
+	if entity == null:
+		print("Unknown entity")
+		return
+	entity.queue_free()
+	_entities.erase(entity_id)
